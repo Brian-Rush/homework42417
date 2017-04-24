eef971924273a6c402d16ee2573c47d92f54af14
Brian Rush and Daniela Trulls <brian@>
2017-04-24 15:21:30 -0700

    add charAt, slice(-1), and toUpperCase

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
-jQuery(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.")
-  });
+// jQuery(document).ready(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.")
+//   });
+//
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+//
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
+//
+var sentence1 = prompt("Write a sentence.")
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
+var userSentence = function(hhh){
+  return (hhh);
+}
 
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-});
+userSentence(sentence1);
+console.log(sentence1);
+
+console.log(sentence1.charAt(0), sentence1.slice(-1));
+
+var firstLast = (sentence1.charAt(0) + sentence1.slice(-1));
+
+var upper = firstLast.toUpperCase();
+
+// var backward = upper.reverse();
+
+
+
+console.log(firstLast);
+console.log(upper);
+// console.log(backward);
